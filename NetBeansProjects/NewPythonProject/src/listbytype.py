dfa1d5f18dbe69ac237b9f4721cb9eb89dbdfe6a
binsina9 <binsina9@gmail.com>
2015-09-11 23:39:28 -0400

    Cleaned up code for readability and comments added

--- NetBeansProjects/NewPythonProject/src/listbytype.py
+++ NetBeansProjects/NewPythonProject/src/listbytype.py
@@ -1,20 +1,21 @@
 fName=str(raw_input("what is your firt name?"))
 lName=str(raw_input("what is your last name?"))
-age=int(raw_input("How do you do pay for transportation this year?"))
+transCost=int(raw_input("How do you do pay for transportation this year?"))
 College=str(raw_input("What is the name of your college or school you attend ?"))
 Graduation=input("How much do you pay for tution this year ?")
-InsuranceNum=input("How much do you pay for insurance deductible per year ?")
-GroupNumber=str(raw_input("what is the name of your insurance carrier"))
+InsurancePay=input("How much do you pay for insurance deductible per year ?")
+InsuranceName=str(raw_input("what is the name of your insurance carrier ?"))
+#variables for user input
 
-
-List =  [(fName,lName,age,College,Graduation,InsuranceNum,GroupNumber)]
-
+List =  [(fName,lName,transCost,College,Graduation,InsurancePay,InsuranceName)]
+#User inputs are returned into a List/Aaray
 Liststrings = list(map(lambda x:(x[0],x[1],x[3],x[6]),List))
 print(" ")
 print (Liststrings)
-
+#using lambda and map tool you can separate the list by string
 Listnumaddition = list(map(lambda x:(x[2]+x[4]+x[5]),List))
 Listnumbers = list(map(lambda x:(x[2],x[4],x[5]),List))
+#using lambda/map tool to separate the list by numbers/and add the integars
 print (Listnumbers)
 print(" ")
 print (fName,lName,"Your Total expensives this year is:",Listnumaddition)        
