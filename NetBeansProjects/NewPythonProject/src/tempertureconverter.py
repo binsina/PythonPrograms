0abe42cde985c1ab00aad73cdf365d37ed717d31
binsina9 <binsina9@gmail.com>
2015-09-16 19:05:40 -0400

    Python programs added

---
+++ NetBeansProjects/NewPythonProject/src/tempertureconverter.py
@@ -0,0 +1,16 @@
+import pymsgbox
+def TemperatureConverter():
+    '''celsius = float(input('Enter degree Celsius: '))
+
+# calculate fahrenheit
+    fahrenheit = (celsius * 1.8) + 32
+    print('%s degree Celsius is equal to %s degree Fahrenheit' %(celsius,fahrenheit))'''
+
+
+    pymsgbox.prompt("What is your name?)
+
+
+
+
+
+TemperatureConverter()
